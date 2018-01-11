45036571da6bdaffca330ee685f9b6464d4f1d02
Louis DeScioli <louis.descioli@gmail.com>
2018-01-11 12:56:47 -0500

    Adds some more life to the navigation. Configures color to come from data

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,9 @@ import sizes from 'style/sizes';
 import Link from 'gatsby-link';
 import BodyClass from 'lib/components/BodyClass';
 import HamburgerToggle from 'lib/components/HamburgerToggle';
+import { shadows } from 'style/snippets';
+import Ripple from 'react-ink';
+import color from 'color';
 
 const bgColor = 'red';
 
@@ -23,6 +26,11 @@ const Button = styled.button`
   width: 50px;
   z-index: 3;
   border: none;
+  cursor: pointer;
+  user-select: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  user-select: none;
+  outline: none;
   @media (min-width: ${sizes.medium}) {
     top: 20px;
     bottom: inherit;
@@ -32,14 +40,16 @@ const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
+  ${props => shadows({ startingElevation: props.isOpen ? 0 : 1 })};
   @media print {
     display: none;
   }
 `;
 
 const Toggle = props => (
-  <Button onClick={props.onClick}>
+  <Button onClick={props.onClick} isOpen={props.isOpen}>
     <HamburgerToggle isOpen={props.isOpen} />
+    <Ripple />
   </Button>
 );
 
@@ -49,43 +59,70 @@ const MenuContainer = styled.nav`
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: ${bgColor};
   z-index: 2;
   opacity: ${props => (props.isOpen ? 1 : 0)};
   transition: opacity 300ms;
   pointer-events: ${props => (props.isOpen ? 'inherit' : 'none')};
-  padding: 0px 5%;
-  a {
-    display: block;
-    color: white;
-    font-size: 2em;
-    @media (min-width: ${sizes.medium}) {
-      font-size: 3em;
-    }
-    text-decoration: none;
-    line-height: 1.1;
-    margin: 15px 0px;
+  background-color: ${props => props.color};
+`;
+
+const ExtMenuLink = styled.a`
+  display: block;
+  color: white;
+  font-size: 2em;
+  @media (min-width: ${sizes.medium}) {
+    font-size: 3em;
+  }
+  text-decoration: none;
+  line-height: 1.1;
+  padding: 15px 5%;
+  transition: background-color 250ms;
+  &:nth-child(${props => props.i + 1}) {
+    background-color: ${props => props.bgColor};
     &:hover {
-      text-decoration: underline;
+      background-color: ${props =>
+        color(props.bgColor)
+          .darken(0.15)
+          .string()};
+    }
+    &:active {
+      background-color: ${props =>
+        color(props.bgColor)
+          .darken(0.3)
+          .string()};
     }
   }
 `;
 
+const IntMenuLink = ExtMenuLink.withComponent(Link);
+
 const MenuLink = props => {
   if (props.url) {
-    return <a href={props.url}>{props.copy}</a>;
+    return (
+      <ExtMenuLink i={props.i} bgColor={props.color} href={props.url}>
+        {props.copy}
+      </ExtMenuLink>
+    );
   }
   return (
-    <Link to={props.path} onClick={props.onClick}>
+    <IntMenuLink
+      to={props.path}
+      i={props.i}
+      bgColor={props.color}
+      onClick={props.onClick}
+    >
       {props.copy}
-    </Link>
+    </IntMenuLink>
   );
 };
 
 const Menu = props => (
-  <MenuContainer isOpen={props.open}>
+  <MenuContainer
+    isOpen={props.open}
+    color={props.links[props.links.length - 1].color}
+  >
     {props.links.map((link, i) => (
-      <MenuLink key={i} onClick={props.onClick} {...link} />
+      <MenuLink key={i} i={i} onClick={props.onClick} {...link} />
     ))}
   </MenuContainer>
 );
