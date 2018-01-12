e1c253bbdeaa2f04a4ca98d03e4b7debde774de5
Louis DeScioli <louis.descioli@gmail.com>
2018-01-12 13:38:17 -0500

    Moving server code around

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const createStoryPages = require('./server/createStoryPages');
+const createStoryPages = require('./src/server/createStoryPages');
 
 exports.createPages = async props => {
   await createStoryPages(props);
