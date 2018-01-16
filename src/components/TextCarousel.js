6c9ce43cf6730dc17b7f73ab0a4252b7a206f78f
Louis DeScioli <louis.descioli@gmail.com>
2018-01-16 13:26:00 -0500

    Refactors TextCarousel to use Transition component instead of CSSTransition

--- src/components/TextCarousel.js
+++ src/components/TextCarousel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { TransitionGroup, Transition } from 'react-transition-group';
 import { fadeInFrom, fadeOutTo } from 'style/animations';
 
 const CarouselContainer = styled.div`
@@ -10,19 +10,19 @@ const CarouselContainer = styled.div`
 
 const CarouselItem = styled.span`
   position: absolute;
-  &.anim-enter-active {
+  &.entering {
     animation: ${fadeInFrom({ x: 15, y: -25 })} 0.8s forwards;
   }
-  &.anim-exit-active {
+  &.exiting {
     animation: ${fadeOutTo({ x: 5, y: 20 })} 0.6s forwards;
   }
 `;
 
 const TextCarousel = props => (
   <TransitionGroup component={CarouselContainer}>
-    <CSSTransition key={props.childKey} timeout={1000} classNames="anim">
-      <CarouselItem>{props.children}</CarouselItem>
-    </CSSTransition>
+    <Transition key={props.childKey} timeout={1000}>
+      {state => <CarouselItem className={state}>{props.children}</CarouselItem>}
+    </Transition>
   </TransitionGroup>
 );
 
