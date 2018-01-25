a9730a839b94e25f7d0ccd93689f94382262147e
Louis DeScioli <louis.descioli@gmail.com>
2018-01-24 20:06:46 -0500

    Changes design of social page, adding spacing & color animations

--- src/pages/social.js
+++ src/pages/social.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { elevate } from 'style/snippets';
-import sizes from 'style/sizes';
+import React from 'react'
+import styled from 'styled-components'
+import { elevate } from 'style/snippets'
+import sizes from 'style/sizes'
+import color from 'color'
 
 const SocialContainer = styled.a`
   color: ${props => props.color};
@@ -15,25 +16,32 @@ const SocialContainer = styled.a`
   min-height: 180px;
   flex: 1 1;
   text-decoration: none;
-  transition: box-shadow 200ms;
+  filter: grayscale(80%);
+  transition: box-shadow, filter;
+  transition-duration: 1.6s, 1.6s;
+  transition-timing-function: ease-in-out, ease-in-out;
   @media (min-width: ${sizes.medium}) {
     min-height: 250px;
   }
+  margin: 3px;
   &:hover,
   &:focus {
+    transition-duration: 0.5s, 0.2s;
+    transition-timing-function: ease-in-out, ease-in-out;
     text-decoration: underline;
     z-index: 1;
     ${elevate(2)};
+    filter: none;
   }
   &:active {
     ${elevate(3)};
   }
-`;
+`
 
 const Description = styled.span`
   font-weight: 300;
   font-size: 1.5em;
-`;
+`
 
 const SocialSquare = props => (
   <SocialContainer
@@ -41,16 +49,17 @@ const SocialSquare = props => (
     bg={props.bg}
     color={props.color}
     title={props.name}
+    border={props.border}
   >
     <Description>{props.description}</Description>
   </SocialContainer>
-);
+)
 
 const Page = styled.main`
   display: flex;
   flex-flow: row wrap;
   min-height: 100vh;
-`;
+`
 
 const SocialPage = props => (
   <Page>
@@ -58,9 +67,9 @@ const SocialPage = props => (
       <SocialSquare key={i} {...edge.node} />
     ))}
   </Page>
-);
+)
 
-export default SocialPage;
+export default SocialPage
 
 export const pageQuery = graphql`
   query SocialQuery {
@@ -76,4 +85,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
