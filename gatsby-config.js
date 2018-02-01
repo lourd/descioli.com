01b2e50342f8eca2cc7d91cd0a34b5e5bcc251cc
Louis DeScioli <louis.descioli@gmail.com>
2018-02-01 16:40:16 -0500

    Tweaks site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Louis R. DeScioli',
-    description: 'The personal site and portfolio of one Louis R DeScioli',
+    description: 'The personal site and portfolio of Louis R. DeScioli',
     keywords: [
       'software engineer',
       'portfolio',
@@ -12,8 +12,8 @@ module.exports = {
       'augmented reality',
       'indie game dev',
       'MIT',
-      'resume'
-    ]
+      'resume',
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'data',
-        path: `${__dirname}/data/`
-      }
+        path: `${__dirname}/data/`,
+      },
     },
     {
       resolve: 'gatsby-transformer-remark',
@@ -37,21 +37,21 @@ module.exports = {
           `gatsby-remark-external-links`,
           {
             resolve: 'my-gatsby-remark-images',
-            options: { maxWidth: 800, addCaptions: true }
+            options: { maxWidth: 800, addCaptions: true },
           },
           'gatsby-remark-copy-linked-files',
           'my-gatsby-remark-youtube',
           // This has to come after the youtube plugin
           'gatsby-remark-responsive-iframe',
-          'gatsby-remark-autolink-headers'
-        ]
-      }
+          'gatsby-remark-autolink-headers',
+        ],
+      },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-53420391-3'
-      }
-    }
-  ]
-};
+        trackingId: 'UA-53420391-3',
+      },
+    },
+  ],
+}
