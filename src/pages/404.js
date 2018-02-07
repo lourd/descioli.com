c3b17d41de01b3f0667f16cbfe67e6321a4eae0c
Louis DeScioli <louis.descioli@gmail.com>
2018-02-07 16:54:16 -0500

    Polishes the 404 page a bit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+const Page = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  text-align: center;
+  flex-flow: column;
+  h1 {
+    font-size: 4em;
+  }
+`
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Page>
+    <h1>Notta</h1>
+    <h2>There's nothing here</h2>
+    <p>The page you are looking does not exist</p>
+    <Link to="/">Head Home</Link>
+  </Page>
 )
 
 export default NotFoundPage
