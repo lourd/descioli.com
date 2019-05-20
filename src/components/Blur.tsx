027798e688dcda4af369ecbf65f152cd918583c4
Louis DeScioli <louis.descioli@gmail.com>
2019-05-19 20:00:42 -0400

    Adds comment about Firefox regression

--- src/components/Blur.tsx
+++ src/components/Blur.tsx
@@ -20,6 +20,9 @@ export default function Blur({
     <div
       css={css`
         transition: filter ${blurred ? transitionIn : transitionOut}s;
+        /* the 0px blur appears to prime chrome and safari for the transition,
+        making for a smooth experience, but it causes a big perf hit on
+        Firefox. Sorry, Firefox! */
         filter: blur(${blurred ? blur : 0}px);
         will-change: filter;
       `}
