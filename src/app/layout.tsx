f25baa4c8eb793dfd351f12f5742629ad949938b
Louis DeScioli <louis@spatial.io>
2024-03-04 18:48:37 -0500

    Adds vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next"
 import { ReactNode, Suspense } from "react"
 
@@ -42,6 +43,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </AppProvider>
         </AutoRefresh>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
