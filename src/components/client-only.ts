79849b3faa683773a86c0087912cff2e736673b2
Louis DeScioli <louis.descioli@gmail.com>
2025-10-25 15:51:01 -0400

    Disables new linter error for client-only

--- src/components/client-only.ts
+++ src/components/client-only.ts
@@ -2,10 +2,16 @@
 
 import { useEffect, useState } from "react"
 
+let isClient = false
+
 export function ClientOnly(props: { children: React.ReactNode }) {
-  const [isMounted, setIsMounted] = useState(false)
+  const [isMounted, setIsMounted] = useState(isClient)
   useEffect(() => {
-    setIsMounted(true)
+    if (!isClient) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect -- Intentional cascading render
+      setIsMounted(true)
+      isClient = true
+    }
   }, [])
   return isMounted ? props.children : null
 }
