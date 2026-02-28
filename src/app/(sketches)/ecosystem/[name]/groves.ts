f283b3c465f9ec764ca2123178255a8ec997162d
Louis DeScioli <louis.descioli@gmail.com>
2026-02-27 20:52:13 -0500

    Sets up correct variables

--- src/app/(sketches)/ecosystem/[name]/groves.ts
+++ src/app/(sketches)/ecosystem/[name]/groves.ts
@@ -4,24 +4,31 @@ import { createEcosystemApi, ParticleApi } from "./ecosystem-methods"
 
 // #region Me
 assert(process.env.PARTICLE_API_TOKEN, "Missing PARTICLE_API_TOKEN")
-const PARTICLE_API_TOKEN = process.env.PARTICLE_API_TOKEN
-export const myParticleApi = createEcosystemApi(PARTICLE_API_TOKEN)
 assert(process.env.MY_ECOSYSTEM_DEVICE_ID, "Missing MY_ECOSYSTEM_DEVICE_ID")
+assert(process.env.ECOSYSTEM_PASSWORD, "Missing ECOSYSTEM_PASSWORD")
 
-export const MY_ECOSYSTEM_DEVICE_ID = process.env.MY_ECOSYSTEM_DEVICE_ID
-const ECOSYSTEM_PASSWORD = process.env.ECOSYSTEM_PASSWORD
-assert(ECOSYSTEM_PASSWORD, "Missing ECOSYSTEM_PASSWORD")
+// Also use it as the key, why not
+const signingKey = new TextEncoder().encode(process.env.ECOSYSTEM_PASSWORD)
 
 // #region Brooks
 
-// TODO: swap for real
-export const BROOKS_ECOSYSTEM_DEVICE_ID = MY_ECOSYSTEM_DEVICE_ID
+assert(
+  process.env.BROOKS_ECOSYSTEM_DEVICE_ID,
+  "Missing BROOKS_ECOSYSTEM_DEVICE_ID"
+)
+assert(
+  process.env.BROOKS_PARTICLE_API_TOKEN,
+  "Missing BROOKS_PARTICLE_API_TOKEN"
+)
+assert(
+  process.env.BROOKS_ECOSYSTEM_PASSWORD,
+  "Missing BROOKS_ECOSYSTEM_PASSWORD"
+)
+const brooksSigningKey = new TextEncoder().encode(
+  process.env.BROOKS_ECOSYSTEM_PASSWORD
+)
 
-// TODO: make and use his token
-const brooksParticleApi = createEcosystemApi(PARTICLE_API_TOKEN)
-
-// Also use it as the key, why not
-const signingKey = new TextEncoder().encode(ECOSYSTEM_PASSWORD)
+// #region Groves
 
 export type GroveDatum = {
   deviceId: string
@@ -31,27 +38,6 @@ export type GroveDatum = {
   signingKey: Uint8Array
   name: string
   description: string
-}
-
-const groves: Partial<Record<string, GroveDatum>> = {
-  lou: {
-    deviceId: MY_ECOSYSTEM_DEVICE_ID,
-    particleApi: myParticleApi,
-    cookie: `eco-session-${MY_ECOSYSTEM_DEVICE_ID}`,
-    password: ECOSYSTEM_PASSWORD,
-    signingKey,
-    name: "Lou's Ecosystem",
-    description: "A new take on controlling my still-living Ecosystem",
-  },
-  brooks: {
-    deviceId: BROOKS_ECOSYSTEM_DEVICE_ID,
-    particleApi: brooksParticleApi,
-    cookie: `eco-session-${BROOKS_ECOSYSTEM_DEVICE_ID}`,
-    password: ECOSYSTEM_PASSWORD,
-    signingKey,
-    name: "Brooks' Ecosystem",
-    description: "Brooks' still-going Grove garden",
-  },
 }
 
 export function getNames(): Record<string, string> {
@@ -69,4 +55,25 @@ export function getGrove(name: string): GroveDatum | Error {
     return new Error(`No grove found for ${name}`)
   }
   return d
+}
+
+const groves: Partial<Record<string, GroveDatum>> = {
+  lou: {
+    deviceId: process.env.MY_ECOSYSTEM_DEVICE_ID,
+    particleApi: createEcosystemApi(process.env.PARTICLE_API_TOKEN),
+    cookie: `eco-session-${process.env.MY_ECOSYSTEM_DEVICE_ID}`,
+    password: process.env.ECOSYSTEM_PASSWORD,
+    signingKey,
+    name: "Lou's Ecosystem",
+    description: "A new take on controlling my still-living Ecosystem",
+  },
+  brooks: {
+    deviceId: process.env.BROOKS_ECOSYSTEM_DEVICE_ID,
+    particleApi: createEcosystemApi(process.env.BROOKS_PARTICLE_API_TOKEN),
+    cookie: `eco-session-${process.env.BROOKS_ECOSYSTEM_DEVICE_ID}`,
+    password: process.env.BROOKS_ECOSYSTEM_PASSWORD,
+    signingKey: brooksSigningKey,
+    name: "Brooks' Ecosystem",
+    description: "Brooks' still-going Grove garden",
+  },
 }
