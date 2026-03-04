2e87f43cbd2f6d7d20e807b758a258064a188bfa
Louis DeScioli <louis.descioli@gmail.com>
2026-03-03 20:40:05 -0500

    Preserve tab when navigating between groves

--- src/app/(sketches)/ecosystem/[name]/grove-select.tsx
+++ src/app/(sketches)/ecosystem/[name]/grove-select.tsx
@@ -1,9 +1,10 @@
 "use client"
 
 import { Route } from "next"
-import { useParams, useRouter } from "next/navigation"
+import { useParams, usePathname, useRouter } from "next/navigation"
 
 export function GroveSelect(props: { options: Record<string, string> }) {
+  const path = usePathname()
   const params = useParams<{ name: string }>()
   const router = useRouter()
   const names = Object.entries(props.options)
@@ -13,7 +14,7 @@ export function GroveSelect(props: { options: Record<string, string> }) {
       defaultValue={params.name}
       onChange={(evt) => {
         const slug = evt.target.value
-        router.push(`/ecosystem/${slug}` as Route)
+        router.push(path.replace(params.name, slug) as Route)
       }}
     >
       {names.map(([slug, name]) => (
